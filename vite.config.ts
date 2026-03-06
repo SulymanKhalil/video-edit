c20a6e26352f43005a645dec67d4a9bbef37dcfe
Sulyman Khalil <sulyman.hkalil@multiconnect.com.hk>
2026-03-06 16:50:18 +0500

    Fix blank page - remove base path config

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
   build: {
-    assetsDir: 'assets'
+    assetsDir: 'assets',
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      }
+    }
   }
 })
